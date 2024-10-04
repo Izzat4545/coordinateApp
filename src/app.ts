588b7375bd4ff55f9be477b8e030729ef53e4f2c
Izzatbek Sultonov <Izzatsultanov77@gmail.com>
2024-10-04 12:05:56 +0500

    add ci/cd

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { handleErrors, handleNotFound } from "./middleware/errorHandler";
 import { authRoutes } from "./route/coordinateRoutes";
+import cors from "cors";
 import express from "express";
 import { requestLogger } from "./middleware/requestLogger";
 
@@ -8,6 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(requestLogger);
 
+const corsOptions = {
+  origin: ["http://devboi.site"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use("/coordinates", authRoutes);
 
 app.use(handleErrors);
