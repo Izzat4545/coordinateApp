f83d7f87c168399c0833b5497c16f0ea13200da1
Izzatbek <55664400+Izzat4545@users.noreply.github.com>
2024-10-04 18:19:48 +0500

    new permission

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(requestLogger);
 
 const corsOptions = {
-  origin: ["http://devboi.site","http://localhost:5173"],
+  origin: ["http://devboi.site","http://justme.uz"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
